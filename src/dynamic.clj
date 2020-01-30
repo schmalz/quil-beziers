ac85ecffb1ec8162e8af8e65809fd91bc3bf5572
schmalz <schmalz@live.co.uk>
2020-01-30 14:20:00 +0000

    Add Bezier curves.

---
+++ src/dynamic.clj
@@ -0,0 +1,69 @@
+(ns dynamic
+  (:require [clojure.pprint :as pp]
+            [quil.core :as q]))
+
+(def colours
+  
+  ; Use saturation and brightness = 90.
+  {:yellow-green-brown {:bg-hue-low 60
+                        :bg-hue-high 90
+                        :shape-fill 30}
+   
+  ; Use saturation = 6, brightness = 100.
+   :floral-white {:bg-hue-low 160
+                  :bg-hue-high 280
+                  :shape-fill 40}
+   
+   ; Use saturation = 32, brightness = 100.
+   :navajo-white {:bg-hue-low 276
+                  :bg-hue-high 156
+                  :shape-fill 36}
+   
+   ; Use saturation = 2, brightness = 100.
+   :snow {:bg-hue-low 0
+          :bg-hue-high 120
+          :shape-fill 240}})
+
+(defn setup
+  []
+  (q/color-mode :hsb 360 100 100 1.0))
+
+(defn paint-gradient
+  [hue-low hue-high]
+  (q/no-stroke)
+  (doseq [y (range 0 800 5)]
+    (q/fill (q/map-range y 0 800 hue-low hue-high)
+            32
+            100)
+    (q/rect 0 y 800 5)))
+
+(defn paint-shape
+  [hue]
+  (q/stroke hue 0 75) ; Mute the hue slightly for the shape's outline.
+  (q/no-fill)
+  (q/begin-shape)
+  (q/vertex (/ (q/width) 2) #_(rand-int (q/width)) (/ (q/height) 2) #_(rand-int (q/height)))
+  (dotimes [_ 3]
+    (q/bezier-vertex (rand-int (q/width)) (rand-int (q/height))
+                     (rand-int (q/width)) (rand-int (q/height))
+                     (rand-int (q/width)) (rand-int (q/height))))
+  (q/end-shape :close))
+
+(defn paint-shapes
+  [hue]
+  (dotimes [_ 3]
+    (paint-shape hue)))
+
+(defn- save-to-disk
+  []
+  (q/save-frame (pp/cl-format nil
+                              "sketches/~d-~2,'0d-~2,'0d-~2,'0d-~2,'0d-~2,'0d.jpeg"
+                              (q/year) (q/month) (q/day) (q/hour) (q/minute) (q/seconds))))
+
+(defn draw
+  []
+  (q/no-loop)
+  (paint-gradient (get-in colours [:snow :bg-hue-low])
+                  (get-in colours [:snow :bg-hue-high]))
+  (paint-shapes (get-in colours [:snow :shape-fill]))
+  (save-to-disk))
